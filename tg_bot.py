74baa8f51d863698980637ee555f4d3bdd992065
Elnar Mengelbaev <elnar_men@mail.ru>
2023-03-03 17:11:29 +0300

    bring code into compliance with PEP 8

--- tg_bot.py
+++ tg_bot.py
@@ -1,35 +1,40 @@
-import logging
 import os
+import logging
 
 from telegram import Update, ForceReply
-from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+
+from telegram.ext import (
+    Updater,
+    CallbackContext,
+    CommandHandler,
+    MessageHandler,
+    Filters)
+
 from dotenv import load_dotenv
 from google.cloud import dialogflow
+from functools import partial
 
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
-load_dotenv()
 logger = logging.getLogger(__name__)
-PROJECT_ID = os.getenv('PROJECT_ID')
-TG_TOKEN = os.getenv('TG_TOKEN')
 
 
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
+        fr'Здравствуйте, чем можем помочь? {user.mention_markdown_v2()}\!',
         reply_markup=ForceReply(selective=True)
     )
 
 
-def detect_intent_texts(update: Update, context: CallbackContext):
+def detect_intent_texts(update: Update, context: CallbackContext, project_id):
     session_id = update.effective_user.id
     language_code = 'ru'
     session_client = dialogflow.SessionsClient()
-    project_id = PROJECT_ID
     session = session_client.session_path(project_id, session_id)
 
     text_input = dialogflow.TextInput(
@@ -43,14 +48,21 @@ def detect_intent_texts(update: Update, context: CallbackContext):
 
 
 def main():
-    updater = Updater(TG_TOKEN)
+    load_dotenv()
+    project_id = os.getenv('PROJECT_ID')
+    TG_TOKEN = os.getenv('TG_TOKEN')
 
+    updater = Updater(TG_TOKEN)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, detect_intent_texts))
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command,
+        partial(detect_intent_texts, project_id=project_id)
+    ))
 
     updater.start_polling()
     updater.idle()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
--- vk_bot.py
+++ vk_bot.py
@@ -33,9 +33,11 @@ if __name__ == "__main__":
     load_dotenv()
     token = os.getenv("VK_TOKEN")
     project_id = os.getenv('PROJECT_ID')
+
     vk_session = vk.VkApi(token=token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
+
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             detect_intent_texts(event, vk_api, project_id)
